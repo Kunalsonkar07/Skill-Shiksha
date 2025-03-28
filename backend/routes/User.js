5000651a7ba073549551170756d835972bf14280
abdulSamad2727 <abdulsamadansari936@gmail.com>
2025-03-28 22:12:20 +0530

    Add authentication routes for login, signup, OTP, and password change

---
+++ backend/routes/User.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const router = express.Router()
+
+const {
+  login,
+  signup,
+  sendotp,
+  changePassword,
+} = require("../controllers/Auth")
+
+
+const { auth } = require("../middlewares/auth")
+
+// Route for user login
+router.post("/login", login)
+
+// Route for user signup
+router.post("/signup", signup)
+
+// Route for sending OTP to the user's email
+router.post("/sendotp", sendotp)
+
+// Route for Changing the password
+router.post("/changepassword", auth, changePassword)
